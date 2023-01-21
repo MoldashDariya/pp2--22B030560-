0910172c2cd234c7f768d4ef96a5c7388cdeffa3
MoldashDariya <123226757+MoldashDariya@users.noreply.github.com>
2023-01-21 18:31:47 +0600

    Add files via upload

--- 5.py
+++ 5.py
@@ -27,3 +27,92 @@ A = "Sally"
 #A will not overwrite a
 print(a)
 print(A)
+#Variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+#Assign Multiple Values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#2
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+#3
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+#Output Variables
+x = "Python is awesome"
+print(x)
+#2
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+#3
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+#4
+x = 5
+y = 10
+print(x + y)
+#5
+x = 5
+y = "John"
+print(x, y)
+#Global Variables
+#1
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+#2
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+#3
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+#4
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+#5
