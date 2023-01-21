d9c2291a41d8c43ecdb3067dd8665a698207eafc
MoldashDariya <123226757+MoldashDariya@users.noreply.github.com>
2023-01-21 18:06:43 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,187 @@
+#1
+print(10 + 5)
+#2
+x = 5
+y = 3
+
+print(x + y)
+#3
+x = 5
+y = 3
+
+print(x - y)
+#4
+x = 5
+y = 3
+
+print(x * y)
+#5
+x = 5
+y = 3
+
+print(x / y)
+#6
+x = 5
+y = 3
+
+print(x % y)
+#7
+x = 5
+y = 3
+
+print(x ** y)
+#8
+x = 5
+y = 3
+
+print(x // y)
+#9
+x = 5
+
+print(x)
+#10
+x = 5
+
+x += 3
+
+print(x)
+#11
+x = 5
+
+x -= 3
+
+print(x)
+#12
+x = 5
+
+x *= 3
+
+print(x)
+#13
+x = 5
+
+x /= 3
+
+print(x)
+#14
+x = 5
+
+x %= 3
+
+print(x)
+#15
+x = 5
+
+x //= 3
+
+print(x)
+#16
+x = 5
+
+x **= 3
+
+print(x)
+#17
+x = 5
+
+x &= 3
+
+print(x)
+#18
+x = 5
+
+x |= 3
+
+print(x)
+#19
+x = 5
+
+x ^= 3
+
+print(x)
+#20
+x = 5
+
+x >>= 3
+
+print(x)
+#21
+x = 5
+
+x <<= 3
+
+print(x)
+#22
+x = 5
+y = 3
+
+print(x == y)
+#23
+x = 5
+y = 3
+
+print(x != y)
+#24
+x = 5
+y = 3
+
+print(x > y)
+#25
+x = 5
+y = 3
+
+print(x < y)
+#26
+x = 5
+y = 3
+
+print(x >= y)
+#27
+x = 5
+y = 3
+
+print(x <= y)
+#28
+x = 5
+
+print(x > 3 and x < 10)
+#29
+x = 5
+
+print(x > 3 or x < 4)
+#30
+x = 5
+
+print(not(x > 3 and x < 10))
+#31
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+
+
+print(x is y)
+
+
+print(x == y)
+#32
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is not z)
+
+
+print(x is not y)
+
+
+print(x != y)
+#33
+x = ["apple", "banana"]
+
+print("banana" in x)
+#34
+x = ["apple", "banana"]
+
+print("pineapple" not in x)
