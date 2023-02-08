5ec88e2faa9d674fe00a7b8e7eaa5642a42e80b3
MoldashDariya <123226757+MoldashDariya@users.noreply.github.com>
2023-02-08 21:21:53 +0600

    Add files via upload

---
+++ week1/1.py
@@ -0,0 +1,45 @@
+#1
+print("Hello, World!")
+
+#2
+username = input("Enter username:")
+print("Username is: " + username)
+
+#3
+username = raw_input("Enter username:")
+print("Username is: " + username)
+
+#Python Syntax
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+
+#Comment
+#This is a comment.
+print("Hello, World!")
+
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
